8a388b7ad0866a1ca4fd542c5b373ee7dd033b94
aidantleak <leakaidan@gmail.com>
2025-02-26 17:50:07 -0500

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Meal
-
+from django.contrib.auth import logout
 def menu(request):
     meals = Meal.objects.all()  # Fetch all menu items from the database
     return render(request, 'orders/menu.html', {'meals': meals})
+
+def home(request):
+    return render(request, "home.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
