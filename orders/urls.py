393d9a7deb389e3038d153eae5fe1db7f1e7f18f
aidantleak <leakaidan@gmail.com>
2025-02-26 17:48:05 -0500

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -4,3 +4,7 @@ from .views import menu  # Ensure views are correctly imported
 urlpatterns = [
     path('menu/', menu, name ='menu'),
 ]
+urlpatterns = [
+    path("", views.home),
+    path("logout", views.logout_view),
+]
