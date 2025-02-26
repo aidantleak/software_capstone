9f47bba2b75cfe5e7d561226aeb19ed4603e2589
aidantleak <leakaidan@gmail.com>
2025-02-26 12:43:18 -0500

    Update models.py
    I am adding sides, so that we can add fries as well as chips as a side.

--- orders/models.py
+++ orders/models.py
@@ -9,5 +9,12 @@ class Meal(models.Model):
 
     def __str__(self):
         return self.name
+        
+  class Side(models.Model):
+    meal = models.ForeignKey(Meal, related_name='sides', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     
+    def __str__(self):
+        return f"{self.name} - {self.price}"  
     
