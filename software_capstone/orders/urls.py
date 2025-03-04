6fb1767aa41c5d6724a1f0d62248c6f2ea1ed307
Nick Criss <bkcriss@eckerd.edu>
2025-03-04 15:39:13 -0500

    Fixed folder structure

--- software_capstone/orders/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import menu  # Ensure views are correctly imported
-
-urlpatterns = [
-    path('menu/', menu, name ='menu'),
-]
--- software_capstone/orders/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Meal
-
-def menu(request):
-    meals = Meal.objects.all()  # Fetch all menu items from the database
-    return render(request, 'orders/menu.html', {'meals': meals})
