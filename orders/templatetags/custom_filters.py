22850ae97d25916d339cb17b12e6a8167946fa9a
Nick Criss <bkcriss@eckerd.edu>
2025-04-07 21:20:09 -0400

    Fix Triton Service

---
+++ orders/templatetags/custom_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def beautify_status(value):
+    """Converts underscores to spaces and capitalizes each word."""
+    return value.replace('_', ' ').title()
+
+
