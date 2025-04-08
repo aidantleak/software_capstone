78f35e7cb2e87230887495aafc60947df6661a44
Your Name <allynielsenn@Allys-MacBook-Pro.local>
2025-04-08 10:25:16 -0400

    reslving merge conflict

--- orders/templatetags/custom_filters.py
+++ orders/templatetags/custom_filters.py
@@ -1,6 +1,12 @@
 from django import template
 
 register = template.Library()
+
+<<<<<<< HEAD
+@register.filter(name='beautify_status')
+def beautify_status(value):
+    return value.replace('_', ' ').title()
+=======
 @register.filter
 def beautify_status(value):
     """Converts underscores to spaces and capitalizes each word."""
